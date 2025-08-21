dafd3fbb52a09af4f9069c893851a0504bd3eec8
Aldert Zomer <aldertzomer@gmail.com>
2025-08-21 11:38:14 +0200

    Benchmark now scores uniq hits and total hits

--- benchmark/generate_benchmark.sh
+++ benchmark/generate_benchmark.sh
@@ -43,12 +43,20 @@ for cut in Eecoli Estaau Emyctu ; do
     done
 done |parallel -j 64
 
-# this thing checks if the information in the header is the same as the prediction. We only check WT reads and Wildtype prediction (TN) and RES reads and Resistance prediction (TP). There will be many false negatives and unknowns as many of the shorter reads do not cover the resistance determining region, therefore these are not scored. False Positives can occur by chance in the reads with mutation.
+# this thing checks if the information in the header is the same as the prediction. this counts unique hits. We only check WT reads and Wildtype prediction (TN) and RES reads and Resistance prediction (TP). There will be many false negatives and unknowns as many of the shorter reads do not cover the resistance determining region, therefore these are not scored. False Positives can occur by chance in the reads with mutation.
 echo "file,TP,TN,FP" > benchmark/final_scores.csv
 find benchmark |grep prot.updated_table_with_scores_and_mutations.tsv |while read output ; do
     echo $output |tr "\n" ","
     cat $output  |sed 's/_0:/\t/' |cut -f 1,3,11 |awk -F'\t' 'BEGIN{OFS="\t"}{split($2,a,"_");if(length(a)>2){$2=a[1];for(i=2;i<=length(a)-2;i++)$2=$2"_"a[i]}print}'|sort|uniq |grep Wildtype |while read drug name result ; do  echo $name |grep "$drug"_WT ; done |wc -l |tr "\n" ","
-    cat $output  |sed 's/_0:/\t/' |cut -f 1,3,11 |awk -F'\t' 'BEGIN{OFS="\t"}{split($2,a,"_");if(length(a)>2){$2=a[1];for(i=2;i<=length(a)-2;i++)$2=$2"_"a[i]}print}'|sort|uniq |grep Resistant |while read drug name result ; do  echo $name |grep "$drug"_RES ; done |wc -l
+    cat $output  |sed 's/_0:/\t/' |cut -f 1,3,11 |awk -F'\t' 'BEGIN{OFS="\t"}{split($2,a,"_");if(length(a)>2){$2=a[1];for(i=2;i<=length(a)-2;i++)$2=$2"_"a[i]}print}'|sort|uniq |grep Resistant |while read drug name result ; do  echo $name |grep "$drug"_RES ; done |wc -l |tr "\n" ","
     cat $output  |sed 's/_0:/\t/' |cut -f 1,3,11 |awk -F'\t' 'BEGIN{OFS="\t"}{split($2,a,"_");if(length(a)>2){$2=a[1];for(i=2;i<=length(a)-2;i++)$2=$2"_"a[i]}print}'|sort|uniq |grep Resistant |while read drug name result ; do  echo $name |grep "$drug"_WT ; done |wc -l
 done >>benchmark/final_scores.csv
 
+# this thing checks if the information in the header is the same as the prediction. This counts all hits. We only check WT reads and Wildtype prediction (TN) and RES reads and Resistance prediction (TP). There will be many false negatives and unknowns as many of the shorter reads do not cover the resistance determining region, therefore these are not scored. False Positives can occur by chance in the reads with mutation.
+echo "file,TP,TN,FP" > benchmark/final_scores2.csv
+find benchmark |grep prot.updated_table_with_scores_and_mutations.tsv |while read output ; do
+    echo $output |tr "\n" ","
+    cat $output  |sed 's/_0:/\t/' |cut -f 1,3,11 |awk -F'\t' 'BEGIN{OFS="\t"}{split($2,a,"_");if(length(a)>2){$2=a[1];for(i=2;i<=length(a)-2;i++)$2=$2"_"a[i]}print}' |grep Wildtype |while read drug name result ; do  echo $name |grep "$drug"_WT ; done |wc -l |tr "\n" ","
+    cat $output  |sed 's/_0:/\t/' |cut -f 1,3,11 |awk -F'\t' 'BEGIN{OFS="\t"}{split($2,a,"_");if(length(a)>2){$2=a[1];for(i=2;i<=length(a)-2;i++)$2=$2"_"a[i]}print}' |grep Resistant |while read drug name result ; do  echo $name |grep "$drug"_RES ; done |wc -l |tr "\n" ","
+    cat $output  |sed 's/_0:/\t/' |cut -f 1,3,11 |awk -F'\t' 'BEGIN{OFS="\t"}{split($2,a,"_");if(length(a)>2){$2=a[1];for(i=2;i<=length(a)-2;i++)$2=$2"_"a[i]}print}' |grep Resistant |while read drug name result ; do  echo $name |grep "$drug"_WT ; done |wc -l
+done >>benchmark/final_scores2.csv
